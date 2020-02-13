8daaaddf5696c1e6596e572c123212b4a609dc91
AllyCat13 <60206950+AllyCat13@users.noreply.github.com>
2020-02-12 22:10:46 -0500

    Create MachinePlayer.java

---
+++ MachinePlayer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleks
+ */
+public class MachinePlayer {
+    public ArrayList<Card> cardInquery = new ArrayList<Card>();
+    private int playerAge = 0;
+ 
+    public void takeTurn()
+    {
+        boolean isPlaying;
+        
+    }
+ 
+	
+    private Card aiLogic()
+    {
+   
+    }
+}
