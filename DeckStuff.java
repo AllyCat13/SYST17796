90e918cff446d0e66bbc276ddeaf08a2f8f9bba5
AllyCat13 <60206950+AllyCat13@users.noreply.github.com>
2020-02-12 22:09:54 -0500

    Create DeckStuff.java

---
+++ DeckStuff.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleks
+ */
+public class DeckStuff {
+    private int _theCount;
+   private Card[] _theHand;
+   private static final int store = 52;
+   private static Random randoRandy = new Random();
+  
+     public DeckStuff()
+   {
+    //to be implemented 
+   }
+   
+   public void buildDeck()
+   } 
+       //to be implemented 
+   }
+   
+   public void injectCard(Card card)
+   {
+       //to be implemented 
+   }
+   public Card eraseValue(int value)
+   {
+         //to be implemented 
+   }
+   public Card eraseChosenCard()
+   {
+      //to be implemented 
+   }
+   public int bookCheckBeforeDeal() 
+   {
+      //to be implemented 
+   }
+   public int getCount(int value)
+   {
+      //to be implemented 
+   }
+   public String toString()
+   {
+       //to be implemented 
+   }  
+
+   class Card
+{
+   private int _theSuit, _theValue;
+   public Card(int value,int suit)
+   {
+      _theValue = value;
+      _theSuit = suit;
+   }
+   public String toString()
+   {
+          
+   }
+   public int getSuit()
+   {
+     
+   }
+   public int getValue()
+   {
+     
+   }
+
+}
+}
