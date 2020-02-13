edcda3c3e83e1156001056aa8271b726218c8c28
AllyCat13 <60206950+AllyCat13@users.noreply.github.com>
2020-02-12 22:11:17 -0500

    Create PlayGoFish.java

---
+++ PlayGoFish.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aleks
+ */
+public class PlayGoFish {
+    static final Random anyVal = new Random();
+    static private ArrayList<Card> theCards;
+    static public Player[] thePlayers;
+ 
+    public static Card goDraw()
+	{
+		
+	}
+ 
+    public static int goDeckSize()
+	{
+	
+        }
+ 
+    public static void main(String[] args)
+    {
+       
+    }
+enum Card 
+{
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+}
+}
